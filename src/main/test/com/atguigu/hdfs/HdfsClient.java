75feb1d79318c154d883a47c964dcfa29e6970bf
Yang <53424623+yn197@users.noreply.github.com>
2024-03-10 15:21:35 +0800

    hdfs的基本操作

--- src/main/test/com/atguigu/hdfs/HdfsClient.java
+++ src/main/test/com/atguigu/hdfs/HdfsClient.java
@@ -2,8 +2,12 @@ package com.atguigu.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -208,5 +212,125 @@ public class HdfsClient {
 
         // 3 关闭资源
         fs.close();
+    }
+
+    /**
+     * HDFS文件上传
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void putFileToHDFS() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop2:9000"), configuration, "root");
+
+        // 2 创建输入流
+        FileInputStream fis = new FileInputStream(new File("d:/大数据/hadoop学习/测试文件/hadoop_test1.txt"));
+
+        // 3 获取输出流
+        FSDataOutputStream fos = fs.create(new Path("/banhua.txt"));
+
+        // 4 流对拷
+        IOUtils.copyBytes(fis, fos, configuration);
+
+        // 5 关闭资源
+        IOUtils.closeStream(fos);
+        IOUtils.closeStream(fis);
+        fs.close();
+    }
+
+    /**
+     * HDFS文件下载
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void getFileFromHDFS() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop2:9000"), configuration, "root");
+
+        // 2 获取输入流
+        FSDataInputStream fis = fs.open(new Path("/banhua.txt"));
+
+        // 3 获取输出流
+        FileOutputStream fos = new FileOutputStream(new File("d:/大数据/hadoop学习/测试文件/hadoop_test1_download2.txt"));
+
+        // 4 流的对拷
+        IOUtils.copyBytes(fis, fos, configuration);
+
+        // 5 关闭资源
+        IOUtils.closeStream(fos);
+        IOUtils.closeStream(fis);
+        fs.close();
+    }
+
+    /**
+     * 定位文件读取-下载第一块
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void readFileSeek1() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop2:9000"), configuration, "root");
+
+        // 2 获取输入流
+        FSDataInputStream fis = fs.open(new Path("/hadoop-2.7.2.tar.gz"));
+
+        // 3 创建输出流
+        FileOutputStream fos = new FileOutputStream(new File("d:/大数据/hadoop学习/测试文件/hadoop-2.7.2.tar.gz.part1"));
+
+        // 4 流的拷贝
+        byte[] buf = new byte[1024];
+
+        for (int i = 0; i < 1024 * 128; i++) {
+            fis.read(buf);
+            fos.write(buf);
+        }
+
+        // 5关闭资源
+        IOUtils.closeStream(fis);
+        IOUtils.closeStream(fos);
+        fs.close();
+    }
+    /**
+     * 定位文件读取-下载第二块
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void readFileSeek2() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop2:9000"), configuration, "root");// 2 打开输入流
+        FSDataInputStream fis = fs.open(new Path("/hadoop-2.7.2.tar.gz"));
+
+        // 3 定位输入数据位置
+        fis.seek(1024 * 1024 * 128);
+
+        // 4 创建输出流
+        FileOutputStream fos = new FileOutputStream(new File("D:/大数据/hadoop学习/测试文件/hadoop-2.7.2.tar.gz.part2"));
+
+        // 5 流的对拷
+        IOUtils.copyBytes(fis, fos, configuration);
+
+        // 6 关闭资源
+        IOUtils.closeStream(fis);
+        IOUtils.closeStream(fos);
     }
 }
